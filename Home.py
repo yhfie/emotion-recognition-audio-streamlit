55d1a658d48f8b7bb6c1172a76c48b7f4343dff1
yhfie <huwaidar5@gmail.com>
2024-06-22 23:40:31 +0700

    first commit

---
+++ Home.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from functions import generate_df, predict_result
+
+from st_audiorec import st_audiorec
+
+acting_script = pd.read_csv('acting-scripts.csv')
+
+# Initialize session state variables
+if 'rand_script' not in st.session_state:
+    acting_script = pd.read_csv('acting-scripts.csv')
+    rand_row = acting_script.sample()
+    st.session_state.rand_script = rand_row["script"].values[0]
+    st.session_state.rand_emotion = rand_row["emotion"].values[0]
+
+# Use session state variables
+st.header(st.session_state.rand_script)
+st.write("Emotion: ", st.session_state.rand_emotion)
+
+# audio_record = st_audiorec()
+audio_upload = st.file_uploader("Upload file")
+
+# if audio_record is not None:
+    # st.audio(audio_record, format='audio/wav')
+
+if audio_upload:
+    predict = st.button("Proceed")
+    if predict:
+        # if audio_record:
+        #     df = generate_df(audio_record)
+        df = generate_df(audio_upload)
+        st.write(df)
+        # result = predict_result(df)
+
+# st.write("result: ", result)
+
---
+++ functions.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import keras
+import pickle
+import librosa
+
+
+# model_file = open('model.pkl', 'rb')
+encoder_file = open('encoder.pkl', 'rb')
+
+# model = pickle.load(open('model.pkl', 'rb'))
+encoder = pickle.load(encoder_file)
+
+# Function to extract MFCC features from audio
+def extract_features(data, sample_rate):
+  # 1. MFCC
+  mfcc = np.mean(librosa.feature.mfcc(y=data, sr=sample_rate, n_mfcc=40).T, axis=0)
+
+  # 2. chroma_stft
+  stft =  np.abs(librosa.stft(data))
+  chroma_stft = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T, axis=0)
+
+  # 3. Mel spectogram
+  mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sample_rate).T, axis=0)
+
+  # 4. zero crossing rate
+  zcr = np.mean(librosa.feature.zero_crossing_rate(y=data, frame_length=2048, hop_length=512).T, axis=0)
+  # zcr = np.squeeze(zcr)
+
+  # 5. RMSE
+  rmse = np.mean(librosa.feature.rms(y=data, frame_length=2048, hop_length=512).T, axis=0)
+  # rmse = np.squeeze(rmse)
+
+  features = np.hstack([mfcc, chroma_stft, mel, zcr, rmse])
+
+  return features
+
+# Function to load audio, extract features, and apply augmentations
+def get_features(path):
+  data, sample_rate = librosa.load(path)
+
+  # Extract features from the base audio
+  base = extract_features(data, sample_rate)
+
+  return pd.DataFrame(base)
+
+def generate_df(path):
+    features_list = []
+    features = get_features(path)
+    features_list.append(features)
+
+    all_features = pd.concat(features_list, ignore_index=True)
+
+    final_df = all_features.T
+    return final_df
+
+def predict_result(data):
+    pred_test = model.predict(data)
+    Y_pred = encoder.inverse_transform(pred_test)
+    return Y_pred
